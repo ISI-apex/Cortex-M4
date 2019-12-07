cc60f524de9a15de2d2dc89fa221b1d1074a68af
Alexei Colin <acolin@isi.edu>
2019-12-07 12:07:36 -0500

    bl0: rename __bss_start/end to __bss_start/end__
    To be consistent with TRCH BM code.

--- trch-bl0/sections.c
+++ trch-bl0/sections.c
@@ -18,8 +18,8 @@
  * aliasing warnings.
  */
 
-char __bss_start[0] __attribute__((section(".__bss_start")));
-char __bss_end[0] __attribute__((section(".__bss_end")));
+char __bss_start__[0] __attribute__((section(".__bss_start__")));
+char __bss_end__[0] __attribute__((section(".__bss_end__")));
 char __image_copy_start[0] __attribute__((section(".__image_copy_start")));
 char __image_copy_end[0] __attribute__((section(".__image_copy_end")));
 char __rel_dyn_start[0] __attribute__((section(".__rel_dyn_start")));
